f7dc1c219a4ddc50ff8c97cb7a8151eaea470abc
JuanDiego110508 <juandiegolr28@gmail.com>
2026-08-26 12:00:28 -0500

    refactor: Se mejoraron estilos de dashboard y perfil

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../features/auth/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.scss']
 })
@@ -14,12 +14,10 @@ export class DashboardComponent {
   private authService = inject(AuthService);
   private router = inject(Router);
   
-  /* Obtenemos el usuario actual para mostrarlo en la vista */
   user = this.authService.getCurrentUser();
 
-  /* Función para cerrar sesión y redirigir al login */
   logout(): void {
     this.authService.logout();
-    this.router.navigate(['/auth/login']);
+    this.router.navigate(['/']);
   }
 }
